c4586da1947d432d34f38bf73405dcce9b97c229
AleCejas <alejandro.06555@gmail.com>
2025-04-29 11:27:35 -0300

    Modificaciones para respuestas en página principal

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -3,6 +3,7 @@ const moviesRouter = require("./moviesRouter");
 
 const router = Router();
 
+router.get("/", (req, res) => res.send("Hello World!"));
 router.use("/movies", moviesRouter);
 
 module.exports = router;
