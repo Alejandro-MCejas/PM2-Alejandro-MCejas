3874518378f50aa74e8e9a466c70f0fe23fa13ea
AleCejas <alejandro.06555@gmail.com>
2025-06-02 17:00:10 -0300

    Eliminación de función para mantener el server activo

--- back/index.js
+++ back/index.js
@@ -1,6 +1,5 @@
 const app = require("./src/server")
 const conDB = require("./src/config/conDB")
-const keepServerAwake = require("./src/utils/keepAlive")
 
 const PORT = process.env.PORT || 3000
 
@@ -9,7 +8,6 @@ async function startServer() {
         await conDB()
         app.listen(PORT, () => {
             console.log("Se está escuchando en el puerto 3000");
-            keepServerAwake()
         })
     } catch (error) {
         console.error('Error al inciar el servidor', error)
